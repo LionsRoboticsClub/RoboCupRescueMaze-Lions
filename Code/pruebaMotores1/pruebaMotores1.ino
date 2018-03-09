3a2c3d6e483c6a68ccc04b0dd370322cb1972074
Loptt <carlos.deg02@hotmail.com>
2018-03-08 20:02:15 -0600

    Added robot code

---
+++ Code/pruebaMotores1/pruebaMotores1.ino
@@ -0,0 +1,188 @@
+#include <EnableInterrupt.h>
+/*---------------------------------------
+Clase Motor
+*/
+
+class Motor
+{
+public:
+    Motor(int, int, int, int);
+  
+    int getEncoderA() {return encoderA;}
+    int getEncoderB() {return encoderB;}
+    unsigned long long getOldPos() {return oldPos;}
+    int getLastStateA() {return lastStateA;}
+    unsigned long long getEncoderPos() {return encoderPos;}
+    
+    void setEncoderPos(unsigned long long pos) {encoderPos = pos;}
+    void setLastStateA(int state) {lastStateA = state;}
+  
+    float calcularVelocidad();
+    void moverAdelante();
+  
+private:
+    int pinAdelante;
+    int pinAtras;
+    int encoderA;
+    int encoderB;
+    float velocidad;
+    volatile unsigned long long encoderPos;
+    volatile int currentState = LOW;
+    volatile int lastStateA  = LOW;
+
+    unsigned long long oldTime;
+    unsigned long long oldPos;
+};
+
+Motor::Motor(int pin1, int pin2, int eA, int eB)
+{
+    pinAdelante = pin1;
+    pinAtras = pin2;
+  
+    encoderA = eA;
+    encoderB = eB;
+    
+    pinMode(pinAdelante, OUTPUT);
+    pinMode(pinAtras, OUTPUT);
+    
+    pinMode(encoderA, INPUT);
+    digitalWrite(encoderA, HIGH);
+    
+    pinMode(encoderB, INPUT);
+    digitalWrite(encoderB, HIGH);
+  
+    velocidad = 0;
+    encoderPos = 0;
+    
+    oldTime = 0;
+    oldPos;
+}
+
+float Motor::calcularVelocidad()
+{
+   unsigned long long newTime = millis();
+  
+   //newTime = newTime % 10000007;
+   velocidad = ((encoderPos - oldPos)*1000)/((float)(newTime-oldTime));
+   oldTime = newTime;
+   oldPos = encoderPos;
+   return velocidad; 
+}
+
+void Motor::moverAdelante()
+{
+    analogWrite(pinAdelante, 150);
+    analogWrite(pinAtras, 0);
+}
+//------------------------------------------------
+
+
+//VARIABLES
+volatile int currentState = LOW;
+volatile int lastStateA  = LOW;
+
+Motor motor1(4,5,19,25);
+Motor motor2(7,6,18,24);
+Motor motor3(9,8,3,23);
+Motor motor4(10,11,2,22);
+
+volatile long motor1Pos = 0;
+volatile long motor2Pos = 0;
+volatile long motor3Pos = 0;
+volatile long motor4Pos = 0;
+
+float velocidadMotor1 = 0;
+float velocidadMotor2 = 0;
+float velocidadMotor3 = 0;
+float velocidadMotor4 = 0;
+
+
+//INTERRUPTS
+void encodeInterruptM1()
+{   
+    currentState = digitalRead(motor1.getEncoderA());
+    if(currentState == HIGH)
+    {
+        motor1Pos++;
+    }
+
+    motor1.setEncoderPos(motor1Pos);
+    motor1.setLastStateA(currentState);
+}
+
+void encodeInterruptM2()
+{
+    currentState = digitalRead(motor2.getEncoderA());
+    if(currentState == HIGH)
+    {
+        motor2Pos++;
+    }
+
+    motor2.setEncoderPos(motor2Pos);
+    motor2.setLastStateA(currentState);
+}
+
+void encodeInterruptM3()
+{
+    currentState = digitalRead(motor3.getEncoderA());
+    if(currentState == HIGH)
+    {
+        motor3Pos++;
+    }
+
+    motor3.setEncoderPos(motor3Pos);
+    motor3.setLastStateA(currentState);
+}
+
+void encodeInterruptM4()
+{
+    currentState = digitalRead(motor4.getEncoderA());
+    if(currentState == HIGH)
+    {
+        motor4Pos++;
+    }
+
+    motor4.setEncoderPos(motor4Pos);
+    motor4.setLastStateA(currentState);
+}
+//----------------------------------------------
+
+
+void setup() {
+
+    enableInterrupt(motor1.getEncoderA(), encodeInterruptM1, CHANGE);
+    enableInterrupt(motor1.getEncoderB(), encodeInterruptM1, CHANGE);
+    enableInterrupt(motor2.getEncoderA(), encodeInterruptM2, CHANGE);
+    enableInterrupt(motor2.getEncoderB(), encodeInterruptM2, CHANGE);
+    enableInterrupt(motor3.getEncoderA(), encodeInterruptM3, CHANGE);
+    enableInterrupt(motor3.getEncoderB(), encodeInterruptM3, CHANGE);
+    enableInterrupt(motor4.getEncoderA(), encodeInterruptM4, CHANGE);
+    enableInterrupt(motor4.getEncoderB(), encodeInterruptM4, CHANGE);
+  
+    Serial.begin(9600);
+}
+
+void loop() {
+ 
+    motor1.moverAdelante();
+    motor2.moverAdelante();
+    motor3.moverAdelante();
+    motor4.moverAdelante();
+    
+    velocidadMotor1 = motor1.calcularVelocidad();
+    velocidadMotor2 = motor2.calcularVelocidad();
+    velocidadMotor3 = motor3.calcularVelocidad();
+    velocidadMotor4 = motor4.calcularVelocidad();
+
+    Serial.println("Vel Motor1:");
+    Serial.println(velocidadMotor1);
+    Serial.println("Vel Motor2:");
+    Serial.println(velocidadMotor2);
+    Serial.println("Vel Motor3:");
+    Serial.println(velocidadMotor3);
+    Serial.println("Vel Motor4:");
+    Serial.println(velocidadMotor4);
+    
+    //delay(1000);
+}
+
